c4f2376e719c2f7c8f7ae7547fe50f49db73fb17
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-12-16 05:58:51 +0100

    Initial Commit

---
+++ PhotosPickerSandbox/PhotosPickerSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosPickerSandboxApp.swift
+//  PhotosPickerSandbox
+//
+//  Created by Zaid Neurothrone on 2022-12-16.
+//
+
+import SwiftUI
+
+@main
+struct PhotosPickerSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
