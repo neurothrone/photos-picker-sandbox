4d906cb71b07d2183a2ede96350a304e11a0df5f
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-12-16 05:59:59 +0100

    finish simple sample

---
+++ PhotosPickerSandbox/AppMain.swift
@@ -0,0 +1,17 @@
+//
+//  AppMain.swift
+//  PhotosPickerSandbox
+//
+//  Created by Zaid Neurothrone on 2022-12-16.
+//
+
+import SwiftUI
+
+@main
+struct AppMain: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
--- PhotosPickerSandbox/ContentView.swift
+++ PhotosPickerSandbox/ContentView.swift
@@ -5,22 +5,48 @@
 //  Created by Zaid Neurothrone on 2022-12-16.
 //
 
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+  @State private var selectedItem: PhotosPickerItem? = nil
+  @State private var selectedImageData: Data? = nil
+  
+  var body: some View {
+    VStack {
+      PhotosPicker(
+        selection: $selectedItem,
+        matching: .images,
+        photoLibrary: .shared()
+      ) {
+        Text("Select a photo")
+      }
+      .onChange(of: selectedItem) { newItem in
+        Task {
+          // Retrive selected asset in the form of Data
+          if let data = try? await newItem?.loadTransferable(type: Data.self) {
+            selectedImageData = data
+          }
         }
-        .padding()
+      }
+      
+      if let selectedImageData,
+         let uiImage = UIImage(data: selectedImageData) {
+        Image(uiImage: uiImage)
+          .resizable()
+          .scaledToFit()
+          .frame(width: 250, height: 250)
+      }
+      
+      Spacer()
     }
+    .padding()
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
+
